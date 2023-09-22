ec932581cb10b47a302356cca2bde43c8ff697b1
Dylan <dylanc176@nycstudents.net>
2023-09-22 16:27:51 -0400

    added introduce() and student1, student2, and student3

--- src/Student.java
+++ src/Student.java
@@ -9,12 +9,15 @@ public class Student {
         grade = studentGrade;
     }
 
-    public introduce() {
+    public void introduce() {
         if (grade < 6) {
-            System.out.println("My name is " + name + "and I am in elementary school");
-        } else if ( ) {
-
+            System.out.println("My name is " + name + " and I am in elementary school!");
+        } else if (grade < 9) {
+            System.out.println("My name is " + name + " and I am in middle school!");
+        } else {
+            System.out.println("My name is " + name + " and I am in college!");
         }
+        System.out.println("I am " + age + " years old!");
     }
 
 }
--- src/StudentRunner.java
+++ src/StudentRunner.java
@@ -1,6 +1,15 @@
 public class StudentRunner {
     public static void main(String[] args) {
+        Student student1 = new Student("Brian", 6, 1);
+        student1.introduce();
+        System.out.println(" ");
 
+        Student student2 = new Student("Dylan", 15, 11);
+        student2.introduce();
+        System.out.println(" ");
+
+        Student student3 = new Student("Lain", 19, 13);
+        student3.introduce();
 
     }
 }
