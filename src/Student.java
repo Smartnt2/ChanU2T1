713036920a42ce12035db3e6f4c7fce9eb24f0a3
BT_1N3_29 <example@bths.edu>
2023-09-22 09:32:12 -0400

    added StudentRunner.java and Student.java

---
+++ src/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private String name;
+    private int age;
+    private int grade;
+
+    public Student(String studentName, int studentAge, int studentGrade) {
+        name = studentName;
+        age = studentAge;
+        grade = studentGrade;
+    }
+
+    public introduce() {
+        if (grade < 6) {
+            System.out.println("My name is " + name + "and I am in elementary school");
+        } else if ( ) {
+
+        }
+    }
+
+}
